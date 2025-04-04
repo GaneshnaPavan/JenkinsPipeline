2b66b211aba3a8b36a705ca89a709dd121d525a4
GaneshnaPavan <ganeshnapavan00@gmail.com>
2025-04-04 12:57:53 +0530

    Add files via upload

---
+++ sonardemo/sonardemo/src/main/java/com/sonar/Loan.java
@@ -0,0 +1,14 @@
+package com.sonar;
+
+public class Loan {
+
+	public Loan() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getEmi() {
+		int x=10;
+		return 99;
+	}
+
+}
---
+++ sonardemo/sonardemo/src/test/java/com/sonar/LoanTest.java
@@ -0,0 +1,41 @@
+package com.sonar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LoanTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testLoan() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEmi() {
+		Loan l=new Loan();
+		assertEquals(99,l.getEmi());
+		//fail("Not yet implemented");
+	}
+
+}
